7f1124b1efecc4311ac546e38341329169817cbb
OscarLarm <118494885+OscarLarm@users.noreply.github.com>
2024-11-27 11:52:22 +0100

    Redesign header

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,10 @@ function App() {
       <div className="hero">
         <div className='hero-content'>
         <div className="hero-left">
-          <img className="tempImg" src="https://www.strasys.uk/wp-content/uploads/2022/02/Depositphotos_484354208_S.jpg" alt="" />
-        </div>
-        <div className="hero-right">
           <div className="hero-text">
           <h1 className='hero-title'><span className='accent'>Oscar Larm</span></h1>
           <hr className='seperator'></hr>
-            <p className="hero-subtitle">I am studying <span className='accent'><br></br>Software Engineering</span> at BTH</p>
+            <p className="hero-subtitle">I am studying <span className='accent'><br></br>Software Engineering</span><br></br> at BTH</p>
             <div className="hero-para">
               <p><i>(Blekinge Institute of Technology)</i></p>
             </div>
@@ -25,6 +22,10 @@ function App() {
               <a href="https://github.com/OscarLarm"><img src={githubLogo} alt="https://github.com/OscarLarm" /></a>
             </div>
           </div>
+        <div className="hero-right">
+          <img className="tempImg" src="https://www.strasys.uk/wp-content/uploads/2022/02/Depositphotos_484354208_S.jpg" alt="" />
+        </div>
+        
         </div>
         </div>
 
