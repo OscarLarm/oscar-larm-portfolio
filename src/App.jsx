b9ca5f42bb06fc1fbd9224f95b963bdb140eb1de
OscarLarm <118494885+OscarLarm@users.noreply.github.com>
2024-11-27 14:46:37 +0100

    Add meta tags, remove unused import in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import githubLogo from './assets/github-mark-white.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
