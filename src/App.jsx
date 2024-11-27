c32a7b3340ce510972b2f19dc86cc75d294d4725
OscarLarm <118494885+OscarLarm@users.noreply.github.com>
2024-11-27 01:20:08 +0100

    Add version1 of hero component

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,30 @@ function App() {
   return (
     <>
       <div className="hero">
-      <div className='heroLogo'>
-          <a href="https://github.com/OscarLarm">
-            <img src={githubLogo} alt="Github link" className='logo'/>
-          </a>
+        <div className='hero-content'>
+        <div className="hero-left">
+          <img className="tempImg" src="https://www.strasys.uk/wp-content/uploads/2022/02/Depositphotos_484354208_S.jpg" alt="" />
         </div>
-        <h1 className='heroText'>Hello! <br></br> My name is Oscar.</h1>
-      </div>
-    </>
+        <div className="hero-right">
+          <div className="hero-text">
+          <h1 className='hero-title'><span className='accent'>Oscar Larm</span></h1>
+          <hr className='seperator'></hr>
+            <p className="hero-subtitle">I am studying <span className='accent'><br></br>Software Engineering</span> at BTH</p>
+            <div className="hero-para">
+              <p><i>(Blekinge Institute of Technology)</i></p>
+            </div>
+            </div>
+            <div className='icon'>
+              <a href="https://github.com/OscarLarm"><img src="./src/assets/github-mark-white.svg" alt="https://github.com/OscarLarm" /></a>
+            </div>
+          </div>
+        </div>
+        </div>
+
+        <div className="testSection">
+
+        </div>
+          </>
   )
 }
 
